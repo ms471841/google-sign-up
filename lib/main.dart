b8eee5f6331966106f0ffb24dfe03d235b79782f
ms471841 <ms471841@.com>
2021-06-07 22:37:18 +0530

    added some comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:googlesign/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
